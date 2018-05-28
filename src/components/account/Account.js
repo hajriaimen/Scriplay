5300d305a09ce12496384ac9867b87859e4a50ec
chalang rasul <chalangr12@gmail.com>
2018-05-28 15:25:47 +0200

    Committing initial commit to the navigation.

---
+++ src/components/account/Account.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Account extends Component{
+    constructor(props){
+      super(props);
+  }
+  
+  render(){
+      return(
+        <div class="row">
+            <div class="col-md-1">
+                Account
+            </div>
+        </div>
+
+    );
+  }
+}
