adcb49f12f83465f36dbfb9aade5bbe449398be9
sparky <hajri.aimen@gmail.com>
2018-06-04 12:55:22 +0100

    fixing cursor (#3)

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
   render() {
     return(
       <Router>
-          <div className="card text-center">
+          <div className="card ">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -41,18 +41,18 @@ class App extends Component {
                     <a class="nav-link" href="Home">Home <span class="sr-only">(current)</span></a>
                   </li>
                   <li class="nav-item active">
-                    <a class="nav-link" href="Account">Account <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="/Instructor">Instructor <span class="sr-only">(current)</span></a>
                   </li>
                   <li class="nav-item active">
-                    <a class="nav-link" href="Account">Account <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="/Student">Student <span class="sr-only">(current)</span></a>
                   </li>
                   <li class="nav-item active">
                     <a class="nav-link" href="Account">Account <span class="sr-only">(current)</span></a>
                   </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
+                <form class="form-inline my-2 my-lg-0 text-center">
                   <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                  <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit" >Search</button>
+                  <button class="btn btn-outline-secondary my-2 my-sm-0 " type="submit" >Search</button>
                 </form>
               </div>
             </nav>
